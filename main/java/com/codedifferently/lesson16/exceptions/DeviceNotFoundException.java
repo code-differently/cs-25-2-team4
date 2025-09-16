26a896ea7e8fabdcac7bba882584dcdec16a794a
Trinitie Jackson <txjackson252@gmail.com>
2025-09-16 16:51:20 -0400

    fix: Delete exceptions directory

--- main/java/com/codedifferently/lesson16/exceptions/DeviceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codedifferently.lesson16.exceptions;
-
-public class DeviceNotFoundException extends RuntimeException {
-    public DeviceNotFoundException(String message) {
-        super(message);
-    }
-}
