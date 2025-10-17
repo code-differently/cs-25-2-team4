86a03b81c516bfa04002cf6189786c44545cdf5c
Trinitie Jackson <txjackson252@gmail.com>
2025-10-17 23:20:20 +0000

    feat: adds css and testing text

--- SmartHomeCapstone/frontend/src/pages/Home/Home.jsx
+++ SmartHomeCapstone/frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'; 
+import "./Home.css";   
 
 export const Home = () => {
   return (
-    <div>
-
+    <div className="home">
+      <h1>Welcome to the Smart Home Management System</h1>
+      <p>Manage and monitor your smart home devices with ease.</p>
     </div>
   );
 };
