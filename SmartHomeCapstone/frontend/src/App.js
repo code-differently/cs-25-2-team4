6a6c56646daea3cc01fbe7f0f903b4b83b2a42b0
Trinitie Jackson <txjackson252@gmail.com>
2025-10-17 22:42:36 +0000

    fix: fixes app routers with a wrapper

--- SmartHomeCapstone/frontend/src/App.js
+++ SmartHomeCapstone/frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Header } from './components/header/Header';
-import { Routes, Route, NavLink, Outlet } from "react-router-dom";
+import { Routes, Route, NavLink, Outlet, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Devices from "./pages/Devices";
@@ -12,10 +12,7 @@ import "./Layout.css";
 function Layout(){
   return(
     <div className="container">
-      <aside className="sidebar">
-        <h2 className="brand">SmartHome</h2>
         <Header />
-      </aside>
       <main className="main"><Outlet /></main>
     </div>
   );
@@ -23,16 +20,18 @@ function Layout(){
 
 export default function App(){
   return(
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
         <Route path="/devices" element={<Devices/>}/>
         <Route path="/devices/add" element={<DeviceAdd/>}/>
         <Route path="/devices/:deviceId/remove" element={<DeviceRemove/>}/>
       </Route>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
+    </BrowserRouter>
   );
 }
