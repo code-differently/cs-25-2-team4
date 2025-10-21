a98701fcbea0b5ba06fb280dd943f8fb01164d68
Toby Evans <teevans252@gmail.com>
2025-10-21 14:31:46 +0000

    chore(routes): add login/register routes with simple App.js

--- SmartHomeCapstone/frontend/src/App.js
+++ SmartHomeCapstone/frontend/src/App.js
@@ -1,36 +1,17 @@
-import './App.css';
-import { Header } from './components/header/Header';
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Devices from "./pages/Devices";
-import DeviceAdd from "./pages/DeviceAdd";
-import DeviceRemove from "./pages/DeviceRemove";
 import NotFound from "./pages/NotFound";
-import "./Layout.css";
-import { Home } from './pages/Home/Home';
 
-function Layout(){
-  return(
-    <div className="container">
-        <Header />
-      <main className="main"><Outlet /></main>
-    </div>
-  );
-}
-
-export default function App(){
-  return(
-      <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />}/>
-            <Route path="/devices" element={<Devices/>}/>
-            <Route path="/devices/add" element={<DeviceAdd/>}/>
-            <Route path="/devices/:deviceId/remove" element={<DeviceRemove/>}/>
-          </Route>
-      <Route path="*" element={<NotFound/>}/>
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/devices" element={<Devices />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
