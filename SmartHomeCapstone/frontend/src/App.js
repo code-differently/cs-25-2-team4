1ed3bba40cdb9f433eee632b4d781e47f1607b58
Danielson A <adjocydanielson@gmail.com>
2025-10-21 22:07:49 -0400

    fix: using trinities home.jsx

--- SmartHomeCapstone/frontend/src/App.js
+++ SmartHomeCapstone/frontend/src/App.js
@@ -1,40 +1,33 @@
-import { Routes, Route, NavLink, Outlet, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Devices from "./pages/Devices.jsx";
-import DeviceAdd from "./pages/DeviceAdd.jsx";
-import DeviceRemove from "./pages/DeviceRemove.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import Home from "./pages/Home/Home.jsx";
+import { Header } from './components/header/Header';
+import { Routes, Route, Outlet } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Devices from "./pages/Devices";
+import DeviceAdd from "./pages/DeviceAdd";
+import DeviceRemove from "./pages/DeviceRemove";
+import NotFound from "./pages/NotFound";
 import "./Layout.css";
-
+import Home from "./pages/Home/Home.jsx";
 function Layout(){
-  return (
+  return(
     <div className="container">
-      <aside className="sidebar">
-        <h2 className="brand">SmartHome</h2>
-        <nav className="nav">
-          <NavLink to="/devices">Devices</NavLink>
-          <NavLink to="/devices/add">Add Device</NavLink>
-        </nav>
-      </aside>
+        <Header />
       <main className="main"><Outlet /></main>
     </div>
   );
 }
 
 export default function App(){
-  return (
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to="/login" replace/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/devices" element={<Devices/>}/>
-        <Route path="/devices/add" element={<DeviceAdd/>}/>
-        <Route path="/devices/:deviceId/remove" element={<DeviceRemove/>}/>
-      </Route>
+  return(
+      <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />}/>
+            <Route path="/devices" element={<Devices/>}/>
+            <Route path="/devices/add" element={<DeviceAdd/>}/>
+            <Route path="/devices/:deviceId/remove" element={<DeviceRemove/>}/>
+          </Route>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   );
