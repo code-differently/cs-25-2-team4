63c7657699ce9908adc15970cb9e337142d57d3b
Danielson A <adjocydanielson@gmail.com>
2025-10-22 00:42:54 -0400

    feat: added sample devices to backend

--- SmartHomeCapstone/backend/src/main/java/com/smarthome/backend/config/DataLoader.java
+++ SmartHomeCapstone/backend/src/main/java/com/smarthome/backend/config/DataLoader.java
@@ -1,7 +1,7 @@
 package com.smarthome.backend.config;
 
-import com.smarthome.backend.entity.Home;
-import com.smarthome.backend.entity.Room;
+import com.smarthome.backend.entity.*;
+import com.smarthome.backend.repository.DeviceRepository;
 import com.smarthome.backend.repository.HomeRepository;
 import com.smarthome.backend.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,8 @@ public class DataLoader implements CommandLineRunner {
         @Autowired private HomeRepository homeRepository;
 
         @Autowired private RoomRepository roomRepository;
+
+        @Autowired private DeviceRepository deviceRepository;
 
         @Override
         public void run(String... args) throws Exception {
@@ -34,9 +36,26 @@ public class DataLoader implements CommandLineRunner {
                         roomRepository.save(bedroom);
                         roomRepository.save(bathroom);
 
+                        // Create sample devices
+                        Light livingRoomLight = new Light("Living Room Light", livingRoom);
+                        livingRoomLight.setBrightness(75);
+                        livingRoomLight.setColorHex("#FFDD44");
+
+                        Thermostat bedroomThermostat = new Thermostat("Bedroom Thermostat", bedroom);
+                        bedroomThermostat.setTargetTemp(72.0);
+
+                        SecurityCamera securityCamera = new SecurityCamera("Security Camera", livingRoom);
+                        securityCamera.setStreamUrl("http://example.com/camera/stream");
+                        securityCamera.setResolution("1080p");
+
+                        deviceRepository.save(livingRoomLight);
+                        deviceRepository.save(bedroomThermostat);
+                        deviceRepository.save(securityCamera);
+
                         System.out.println("Sample data loaded successfully!");
                         System.out.println("Created homes: " + homeRepository.count());
                         System.out.println("Created rooms: " + roomRepository.count());
+                        System.out.println("Created devices: " + deviceRepository.count());
                 }
         }
 }
