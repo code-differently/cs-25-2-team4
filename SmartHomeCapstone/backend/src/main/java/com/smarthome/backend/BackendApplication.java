d1ca31e8a33128d496668f675b731218defd2ba6
Danielson A <adjocydanielson@gmail.com>
2025-10-26 00:27:33 -0400

    chore: spotlessApply

--- SmartHomeCapstone/backend/src/main/java/com/smarthome/backend/BackendApplication.java
+++ SmartHomeCapstone/backend/src/main/java/com/smarthome/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.smarthome.backend;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +11,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
