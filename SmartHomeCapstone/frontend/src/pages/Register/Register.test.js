14b84c41538835ca8814a8af2336d34eb6265606
Trinitie Jackson <txjackson252@gmail.com>
2025-10-27 04:19:48 +0000

    fix: fixes Register file and test to pass

--- SmartHomeCapstone/frontend/src/pages/Register/Register.test.js
+++ SmartHomeCapstone/frontend/src/pages/Register/Register.test.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Register from './Register';
